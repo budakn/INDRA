3eef5cf210490c040ae642a15b40f805c43cc5f4
pagreene <patrick.anton.greene@gmail.com>
2018-02-22 18:12:58 -0500

    Fix sif tests.

--- indra/tests/test_sif_assembler.py
+++ indra/tests/test_sif_assembler.py
@@ -3,6 +3,7 @@ from builtins import dict, str
 from indra.assemblers import SifAssembler
 from indra.statements import *
 
+
 def test_simple_assembly():
     st1 = Activation(Agent('a'), Agent('b'))
     st2 = Inhibition(Agent('a'), Agent('c'))
@@ -11,11 +12,12 @@ def test_simple_assembly():
     assert(len(sa.graph.nodes()) == 3)
     assert(len(sa.graph.edges()) == 2)
 
+
 def test_evidence_assembly():
     ev1 = Evidence(pmid='1')
     ev2 = Evidence(pmid='2')
     ev3 = Evidence(pmid='3')
-    ev4 = Evidence(pmid='4')
+    Evidence(pmid='4')
     st1 = Activation(Agent('a'), Agent('b'), evidence=[ev1])
     st2 = Inhibition(Agent('a'), Agent('c'), evidence=[ev1, ev2, ev3])
     sa = SifAssembler([st1, st2])
